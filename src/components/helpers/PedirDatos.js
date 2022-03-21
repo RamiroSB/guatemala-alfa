baee934c7db0b34dcc2344f957e97426ca090c90
Ramiro <ramiro_salerno@solutionbox.com.ar>
2022-03-21 16:41:23 -0300

    Modificacion helpers comentarios

--- src/components/helpers/PedirDatos.js
+++ src/components/helpers/PedirDatos.js
@@ -1,10 +1,6 @@
 import { Empleados } from '../Data/Empleados'
 
 /* Funcion con promesa para pedir datos del array de objetos */
-<<<<<<< HEAD
-=======
-
->>>>>>> e2fe7b325fe4ff5ad87fbe165aa22fcf04415ad5
 export const PedirDatos = () => {
   return new Promise ((resolve, reject)=> {
       setTimeout(()=>{
--- src/components/helpers/PedirDatosStock.js
+++ src/components/helpers/PedirDatosStock.js
@@ -1,10 +1,6 @@
 import { Stock } from '../Data/Stock'
 
 /* Funcion con promesa para pedir datos del array de objetos */
-<<<<<<< HEAD
-=======
-
->>>>>>> e2fe7b325fe4ff5ad87fbe165aa22fcf04415ad5
 export const PedirDatosStock = () => {
   return new Promise ((resolve, reject)=> {
       setTimeout(()=>{
