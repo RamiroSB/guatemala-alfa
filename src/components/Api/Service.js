924731b879c44ac770154c529e9b52b5c0688e7f
Ramiro <ramiro_salerno@solutionbox.com.ar>
2022-05-04 15:50:19 -0300

    Modificacion Api

--- src/components/Api/Service.js
+++ src/components/Api/Service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = 'http://localhost:3333/articulos'
+const API = 'http://localhost:3333/articulos' 
 
 export const getProducts = async () => {
 /*     console.log (await axios.get (`${API}`)) */
